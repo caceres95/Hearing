01a42c5ee8f27693e9b01c3d040edf28662157a3
Andrés Castro <Andrs Castro>
2016-10-19 17:14:47 -0500

    mvp 1 (chafo)

--- NRF__LCD/NRF__LCD.ino
+++ NRF__LCD/NRF__LCD.ino
@@ -13,17 +13,12 @@ byte direccion[5] = {'c', 'a', 'n', 'a', 'l'};
 //creamos el objeto radio (NRF24L01)
 RF24 radio(CE_PIN, CSN_PIN);
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
 //Hasta aquí hemos declarado la librería para la LCD y los pines por donde le va a entrar la información.
 
+bool xy = true;
 int dato;
-
-float accel;
-float  peraccel;
-float giron;
-float girof;
-float giro;
 
 //vector para los datos recibidos
 float datos[3];
@@ -50,11 +45,6 @@ void setup() {
 
 void loop() {
 
-  
-  lcd.setCursor(2,0);
-  lcd.print("Palabra");
-  
-
   if (Serial.available()); { //si la comunicacion esta habilitada
 
     dato = Serial.read();  //asignar el valor recibido a la variable dato
@@ -66,18 +56,6 @@ void loop() {
     {
       //Leemos los datos y los guardamos en la variable datos[]
       radio.read(datos, sizeof(datos));
-
-      if(datos[0] <= -1.5)
-        datos[0] = -1.5;
-
-      if(1.5 <= datos[0])
-        datos[0] = 1.5;
-
-      if(datos[1] <= -1.5)
-        datos[0] = -1.5;
-
-      if(1.5 <= datos[1])
-        datos[0] = 1.5;
 
       //reportamos por el puerto serial los datos recibidos
       //Serial.print("Dato0= " );
@@ -93,76 +71,38 @@ void loop() {
     {
       Serial.println("No hay datos de radio disponibles");
     }
-
-    //4 y 5 llanta izquierda
-    //  4- y 5+  avanza
-    //  4+ y 5- retrocede
-    //6 y 7 llanta derecha
-    //  6- y 7+  avanza
-    //  6+ y 7- retrocede
-
-    if(datos[0] < 0)    //Carro Retrocede
+    
+    if(((datos[0] < 1 && datos[0] > -1) && (xy)) || ((datos[1] > -1 && datos[1] < 1)) && (xy == false))
     {
-      if(datos[1] < 0)  //Derecha
-      {
-        accel = datos[0]*170*-1;
-        peraccel = accel/255;
-        giron = datos[1]/1.5*-1;
-        girof = datos[2]/1.5*-1;
-        giro = (peraccel*girof) + ((1-peraccel)*giron);
-        
-        analogWrite(6, accel);
-        analogWrite(7, 0);
-        analogWrite(4, accel*(1-giro));
-        analogWrite(5, 0);
-
-      }
-      
-      else              //Izquierda
-      {
-        accel = datos[0]*170*-1;
-        peraccel = accel/255;
-        giron = datos[1]/1.5;
-        girof = datos[2]/1.5;
-        giro = (peraccel*girof) + ((1-peraccel)*giron);
-        
-        analogWrite(6, accel*(1-giro));
-        analogWrite(7, 0);
-        analogWrite(4, accel);
-        analogWrite(5, 0);
-      }
+      lcd.clear();
     }
 
-    else                //Carro avanza
+    if(datos[0] > 1)    //Carro Retrocede
     {
-      if(datos[1] < 0)  //Derecha
-      {
-        accel = datos[0]*170;
-        peraccel = accel/255;
-        giron = datos[1]/1.5*-1;
-        girof = datos[2]/1.5*-1;
-        giro = (peraccel*girof) + ((1-peraccel)*giron);
-        
-        analogWrite(6, 0);
-        analogWrite(7, accel);
-        analogWrite(4, 0);
-        analogWrite(5, accel*(1-giro));
-      }
-      
-      else              //Izquierda
-      {
-        accel = datos[0]*170;
-        peraccel = accel/255;
-        giron = datos[1]/1.5;
-        girof = datos[2]/1.5;
-        giro = (peraccel*girof) + ((1-peraccel)*giron);
-        
-        analogWrite(6, 0);
-        analogWrite(7, accel*(1-giro));
-        analogWrite(4, 0);
-        analogWrite(5, accel);
-      }
+      lcd.setCursor(0,0);
+      lcd.print("me");
+      xy = true;
+    }
+    else if(datos[0] < -1)
+    {
+      lcd.setCursor(0,0);
+      lcd.print("Hola");
+      xy = true;
+    }
+    else if(datos[1] < -1)
+    {
+      lcd.setCursor(0,0);
+      lcd.print("llamo");
+      xy = false;
+    }
+    else if(datos[1] > 1)
+    {
+      lcd.setCursor(0,0);
+      lcd.print("Castro");
+      xy = false;
     }
    } 
+   
+   delay(100);
    
  }
